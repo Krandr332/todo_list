a0b42453ccdf2656f4f247d444395b28404a2279
Андрюша <57506848+Krandr332@users.noreply.github.com>
2024-05-23 18:30:15 +0300

    релиз тестового задания

--- app/config.py
+++ app/config.py
@@ -1,5 +1,5 @@
 import os
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:@localhost/todo_list'
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:QweAsdZxc1234@localhost/todo_list'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
